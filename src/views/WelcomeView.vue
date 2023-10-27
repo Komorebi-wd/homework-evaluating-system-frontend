f92561a2f59ba9f1f53ad9262db1dfa181ca5bea
Komorebi-wd <2504386283@qq.com>
2023-10-27 16:38:28 +0800

    init

---
+++ src/views/WelcomeView.vue
@@ -0,0 +1,40 @@
+<template>
+    <div style="width: 100vw;height: 100vh;overflow: hidden;display: flex">
+        <div style="flex: 1">
+            <el-image style="width: 100%;height: 100%" fit="cover"
+                      src="https://www.cacos.com/uploadfile/2019/0515/20190515112852500.jpg"/>
+        </div>
+        <div class="welcome-title">
+            <div style="font-size: 30px;font-weight: bold">欢迎使用学生作业互评平台</div>
+            <div style="margin-top: 10px">本系统可管理学生作业并相互评阅。</div>
+            <div style="margin-top: 5px">为了系统权限安全，请保管好您的账号密码。</div>
+        </div>
+        <div class="right-card">
+            <router-view v-slot="{ Component }">
+                <transition name="el-fade-in-linear" mode="out-in">
+                    <component :is="Component" style="height: 100%"/>
+                </transition>
+            </router-view>
+        </div>
+    </div>
+</template>
+
+<script setup>
+
+</script>
+
+<style scoped>
+.right-card {
+  width: 400px;
+  z-index: 1;
+  background-color: var(--el-bg-color);
+}
+
+.welcome-title {
+    position: absolute;
+    bottom: 30px;
+    left: 30px;
+    color: white;
+    text-shadow: 0 0 10px black;
+}
+</style>
